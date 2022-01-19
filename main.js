eda46e02ea40a109e46e57a6cced4426c997f5de
Eugene <iam090991@gmail.com>
2022-01-19 09:49:01 +0200

    Start app

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-let i = false,
-		f = false;
+'use strict'
 
-console.log( i && f )
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+		film1 = prompt('Один из последних просмотренных фильмов', ''),
+		filmGrade1 = prompt('На сколько оцените его?', ''),
+		film2 = prompt('Один из последних просмотренных фильмов', ''),
+		filmGrade2 = prompt('На сколько оцените его?', '');
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+}
+
+personalMovieDB.movies.film1 = filmGrade1;
+personalMovieDB.movies.film2 = filmGrade2;
+
+console.log(personalMovieDB);
