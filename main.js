4c79fc0f1fd686e70d36a6f3bb49825e423f48f2
Eugene <iam090991@gmail.com>
2022-01-20 11:56:33 +0200

    loop add

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
 'use strict'
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
-		film1 = prompt('Один из последних просмотренных фильмов', ''),
-		filmGrade1 = prompt('На сколько оцените его?', ''),
-		film2 = prompt('Один из последних просмотренных фильмов', ''),
-		filmGrade2 = prompt('На сколько оцените его?', '');
-
 const personalMovieDB = {
-	count: numberOfFilms,
+	count: '',
 	movies: {},
 	actors: {},
 	genres: [],
 	privat: false
 }
 
-personalMovieDB.movies.film1 = filmGrade1;
-personalMovieDB.movies.film2 = filmGrade2;
+personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+for(let i = 0; i < 2; i++){
+	const film = prompt('Один из последних просмотренных фильмов', ''),
+			grade = prompt('На сколько оцените его?', '');
+
+	if(film != '' && grade != '' && film != null && grade != null && film.length > 50){
+		personalMovieDB.movies[film] = grade;
+		console.log('done');
+	}else{
+		console.log('error');
+		i--;
+	}
+}
+
+if(personalMovieDB.count < 10){
+	console.log('Мало фильмов');
+}else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+	console.log('Вы классический зритель');
+}else if(personalMovieDB.count > 30){
+	console.log('Вы киноман');
+}else{
+	console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
