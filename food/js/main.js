90b248031640d3078177d4152f8b8e97da5add8b
Eugene <iam090991@gmail.com>
2022-02-18 10:18:48 +0200

    food, display item width class

--- food/js/main.js
+++ food/js/main.js
@@ -59,4 +59,66 @@ document.addEventListener('DOMContentLoaded', function(){
 		}
 	}
 
+
+	/*
+	* Использование классов для карточек
+	*/
+
+	class FoodItem{
+		constructor(img, alt, title, description, price, parentSelector){
+			this.img = img;
+			this.alt = alt;
+			this.title = title;
+			this.description = description;
+			this.price = price;
+			this.parentSelector = parentSelector;
+		}
+		render(){
+			let element = document.createElement('div');
+
+			element.classList.add('menu__item');
+			
+			element.innerHTML = `
+				<img src="${this.img}" alt="${this.alt}">
+				<h3 class="menu__item-subtitle">${this.title}</h3>
+				<div class="menu__item-descr">${this.description}</div>
+				<div class="menu__item-divider"></div>
+				<div class="menu__item-price">
+						<div class="menu__item-cost">Цена:</div>
+						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+				</div>
+			`;
+
+			document.querySelector(this.parentSelector).append(element);
+		}
+	}
+
+	const vegy = [
+								'img/tabs/vegy.jpg', 
+								'vegy', 
+								'Меню "Фитнес"',
+								'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+								229,
+								'.menu__field .container'
+							],
+				elite = [
+								'img/tabs/elite.jpg', 
+								'elite', 
+								'Меню “Премиум”',
+								'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+								550,
+								'.menu__field .container'
+							],
+				post = [
+								'img/tabs/post.jpg', 
+								'post', 
+								'Меню "Постное"', 
+								'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+								430,
+								'.menu__field .container'
+							];
+	
+	new FoodItem(...vegy).render();
+	new FoodItem(...post).render();
+	new FoodItem(...elite).render();
 });
